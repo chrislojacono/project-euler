6fd30b5b165178bfcfed22ba31b94971d62e16fa
Chris LoJacono <clojacono92@gmail.com>
2021-02-19 12:30:27 -0600

    multiple pointers

--- src/Components/MultiplePointers.js
+++ src/Components/MultiplePointers.js
@@ -12,3 +12,19 @@ function sumZero(arr){
 
 sumZero([-4,-3,-2,-1,0,1,2,5])
 //O(n^2) checks first then loops through all others --- Only works on linear arrays/sorted
+
+function sumZeroBetter(array){
+    let left = 0;
+    let right = array.length - 1;
+    //failsafe so you dont get 0-0 which would be a false positive
+    while(left < right){
+        let sum = array[left] + array[right];
+        if(sum === 0){
+            return [array[lerft], array[right]];
+        }else if(sum > 0){
+            right--;
+        }else{
+            left++
+        }
+    }
+}
