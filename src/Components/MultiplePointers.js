54887a09514fa21478082a95bc219e2205ec74ce
Chris LoJacono <clojacono92@gmail.com>
2021-02-19 12:30:13 -0600

    done

---
+++ src/Components/MultiplePointers.js
@@ -0,0 +1,14 @@
+//Naive Verison
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+
+sumZero([-4,-3,-2,-1,0,1,2,5])
+//O(n^2) checks first then loops through all others --- Only works on linear arrays/sorted
