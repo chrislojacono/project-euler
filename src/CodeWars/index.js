24f41be02792bea693936e1f8f7c333db4ef35bd
Chris LoJacono <clojacono92@gmail.com>
2021-02-25 14:44:48 -0600

    best answer

--- src/CodeWars/index.js
+++ src/CodeWars/index.js
@@ -33,3 +33,17 @@ function longestConsec(strarr, k) {
     }
   return longString
 }
+
+
+//This was the top answer 0(n)
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
