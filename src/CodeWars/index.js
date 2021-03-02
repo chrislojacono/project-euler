e16dfa9e48fd12ef13f9c709122684cc8f72add9
Chris LoJacono <clojacono92@gmail.com>
2021-03-02 11:04:33 -0600

    frequency counter

--- src/CodeWars/index.js
+++ src/CodeWars/index.js
@@ -146,7 +146,7 @@ function solution(string) {
 }
 
 
-// complete the function
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
 function highestRank(arr){
   let collection = {}
   for(let val in arr){
