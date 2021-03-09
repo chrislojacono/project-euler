92bcdb31e1ee7cb068c6d6d75708f9c0ee038207
Chris LoJacono <clojacono92@gmail.com>
2021-03-09 17:26:36 -0600

    stuck on:

---
+++ src/CodeWars/stuckOn.js
@@ -0,0 +1,38 @@
+/*This is the first part. You can solve the second part here when you are done with this. Multiply two numbers! Simple!
+
+The arguments are passed as strings.
+The numbers may be way very large
+Answer should be returned as a string
+The returned "number" should not start with zeros e.g. 0123 is invalid
+Note: 100 randomly generated tests!
+*/
+
+function toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+          x *= Math.pow(10,e-1);
+          x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+          e -= 20;
+          x /= Math.pow(10,e);
+          x += (new Array(e+1)).join('0');
+      }
+    }
+    return x;
+  }
+  
+  function multiply(a, b)
+  {
+    let numA = parseInt(a);
+    let numB = parseInt(b);
+    let newArray = [numA, numB];
+    let answer = newArray.reduce((c, d) => c * d)
+    let num = toFixed(answer)
+    let stringAnswer = num.toString()
+  
+    return stringAnswer
+  }
