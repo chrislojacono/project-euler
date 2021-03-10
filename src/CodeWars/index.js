a712196bb79d064ef4d7a76c0a09d172379e15f2
Chris LoJacono <clojacono92@gmail.com>
2021-03-09 21:05:37 -0600

    comment

--- src/CodeWars/index.js
+++ src/CodeWars/index.js
@@ -289,3 +289,10 @@ if(finalString[0] == '-'){
 }
 return finalString
 }
+ /*
+ Each exclamation mark weight is 2; Each question mark weight is 3. Put two string left and right to the balance, Are they balanced?
+
+If the left side is more heavy, return "Left"; If the right side is more heavy, return "Right"; If they are balanced, return "Balance".
+ */
+
+
