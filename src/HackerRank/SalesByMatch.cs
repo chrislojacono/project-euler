0d626b0cb2ce1969c0c9ac3d46b88f238c4bebe6
Chris LoJacono <clojacono92@gmail.com>
2021-03-11 20:12:41 -0600

    hackerrank

--- src/HackerRank/SalesByMatch.cs
+++ src/HackerRank/SalesByMatch.cs
@@ -19,3 +19,20 @@ class Solution {
     }
 
     //Function is too slow
+
+    //THIS WAS THE ANSWER
+        static int sockMerchant(int n, int[] ar) {
+    var colors = new List<int>();
+    int pairs = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!colors.Contains(ar[i])) {
+            colors.Add(ar[i]);
+        } else {
+            pairs++;
+            colors.Remove(ar[i]);
+        }
+    }
+
+        return pairs;
+    }
